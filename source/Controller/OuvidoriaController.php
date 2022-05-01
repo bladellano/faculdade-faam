1abb98d5c7b7f534b8139334da4a6800ba154344
Caio <bladellano@gmail.com>
2022-05-01 16:49:23 -0300

    Adicionando emails da ouvidoria

--- source/Controller/OuvidoriaController.php
+++ source/Controller/OuvidoriaController.php
@@ -13,43 +13,43 @@ class OuvidoriaController extends Controller
     {
     }
 
-	public function index()
-	{
-		$respostas = \Source\Model\Ouvidoria::getRespostas();
+    public function index()
+    {
+        $respostas = \Source\Model\Ouvidoria::getRespostas();
 
-		$page = new PageAdmin();
-		$page->setTpl("ouvidoria", array(
-			"respostas" => $respostas,
-		));
-		exit;
-	}
+        $page = new PageAdmin();
+        $page->setTpl("ouvidoria", array(
+            "respostas" => $respostas,
+        ));
+        exit;
+    }
 
     public function store(Request $request)
     {
         $lastId = (new Sql)->insert('ouv_respostas', $request->getParsedBody());
 
         if ($lastId) {
+            $resposta = \Source\Model\Ouvidoria::getRespostas(["id"=>$lastId]);
+            $resposta = $resposta[0];
 
-			$resposta = \Source\Model\Ouvidoria::getRespostas(["id"=>$lastId]);
-			$resposta = $resposta[0];
+            // ENVIAR EMAILS
+            $addAddress = [
+                "bladellano@gmail.com",
+                "direcao@faam.com.br",
+				"ouvidoria@faam.com.br",
+				"dir.academica@faam.com.br"
+            ];
 
-			// ENVIAR EMAILS
-			$addAddress = [
-				"bladellano@yahoo.com.br",
-				"bladellano@gmail.com",
-				"dellanosites@gmail.com",
-			];
-			
-			$mailer = new Mailer(
-				'ouvidoria@faam.com.br',
-				'Ouvidoria Faam',
-				"Questionário Ouvidoria Faam", //Assunto
-				"email-resposta-ouvidoria", //Template
-				$resposta,
-				$addAddress
-			);
+            $mailer = new Mailer(
+                'ouvidoria@faam.com.br',
+                'Ouvidoria Faam',
+                "Questionário Ouvidoria Faam", //Assunto
+                "email-resposta-ouvidoria", //Template
+                $resposta,
+                $addAddress
+            );
 
-			$mailer->send();
+            $mailer->send();
 
             print(json_encode([
                 'success' => true,
